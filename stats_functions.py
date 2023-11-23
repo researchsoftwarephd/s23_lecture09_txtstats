af0ddebb6738ff2c1ff5db1aa17c8f8aa911390e
Nuno Fachada <nuno.fachada@ulusofona.pt>
2023-11-23 00:09:42 +0000

    Add documentation to module

--- stats_functions.py
+++ stats_functions.py
@@ -1,9 +1,27 @@
+"""
+Functions for calculating text statistics.
+"""
+
 from typing import Any
 
+
 def text_stats(s: str) -> dict[str, Any]:
+    """Generates statistics for the specified text.
+
+    Args:
+      s:
+        The *text* from which to generate statistics.
+
+    Returns:
+      A dictionary containing the generated text statistics.
+    """
 
     wc = len(s.split())
-    lc = len(s.split('\n'))
+    lc = len(s.split("\n"))
     cc = len(s)
 
-    return {'word_count': wc, 'line_count': lc, 'char_count': cc}
+    return {"word_count": wc, "line_count": lc, "char_count": cc}
+
+
+if __name__ == "__main__":
+    print(text_stats("Hello everyone!\nBye now!"))
